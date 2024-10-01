23c1010abed917dcc1c53b8767fcf1783c24396c
G-1998 <gauravnic76@gmail.com>
2024-10-01 23:40:59 +0530

    add

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -8,6 +8,10 @@ import image1 from './Image/Image1.jpg';
 import image2 from './Image/Image2.jpg';
 import image3 from './Image/Image3.jpg';
 import background from './Image/background.jpg';
+import room from './Image/livingroom.jpg';
+import city from './Image/mandi.jpg';
+import city1 from './Image/sundernagar.jpg';
+import city2 from './Image/iitkamand.jpg';
 
 const Home = () => {
   const { ref: sliderRef, inView: sliderInView } = useInView({
@@ -24,7 +28,7 @@ const Home = () => {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 1,
+    slidesToShow: -1,
     slidesToScroll: 1,
     centerMode: true,
     centerPadding: '60px',
@@ -62,6 +66,57 @@ const Home = () => {
         </div>
       </section>
 
+
+      <section ref={sectionRef} className={`containersection2 background-container ${sectionInView ? 'slide-in' : ''}`}>
+  <div className="parent1">
+    <div className="available-container">  
+    <div className="room-info">    
+        <h1><u>Room Available In</u></h1>
+        </div>
+        <div className='child1'>  
+        <div className="room-card1">
+          <div className="room-card1-content">
+            <img src={city} alt="Mandi" />
+            <div className="overlay-content transbox1">
+              <h1 className="overlay-message animated-text">Mandi</h1>
+            </div>
+          </div>        
+        </div>
+      </div>
+    </div>
+    <div className="upcoming-container">
+    <div className="room-info">      
+      <h1><u> Upcoming Event In</u></h1>
+      </div>
+        <div className='child1'>     
+        <div className="room-card1">
+          <div className="room-card1-content">
+            <img src={city1} alt="Sundernagar" />
+            <div className="overlay-content transbox1">
+              <h1 className="overlay-message animated-text">Sundernagar</h1>
+            </div>
+          </div>
+        </div>
+
+        <div className="room-card1">
+          <div className="room-card1-content">
+            <img src={city2} alt="IIT Kamand" />
+            <div className="overlay-content transbox1">
+              <h1 className="overlay-message animated-text">IIT Kamand</h1>
+            </div>
+           
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+
+
+
+
       {/* Slider Section */}
       <section className={`slider-container ${sliderInView ? 'slide-in' : ''}`}>
         <Slider {...settings}>
@@ -79,8 +134,24 @@ const Home = () => {
 
       {/* Additional Section */}
       <section ref={sectionRef} className={`containersection1 ${sectionInView ? 'slide-in' : ''}`}>
-        <div className="main1">hello</div>
-        <div className="main2">hello</div>
+        <div className="main1">
+          <div className="text">
+            <h1><u>Why We use this Platform ?</u></h1>
+            The Easy Room Finder platform provides a hassle-free, 
+            efficient, and secure way for individuals to find rooms 
+            that meet their requirements, whether they are looking for
+             affordability, convenience, or a specific location. 
+             It helps both room seekers and landlords connect in a 
+             streamlined manner, saving time, effort, and resources for
+             both parties.
+
+          </div>
+        </div>
+        <div className="main2">
+          <div className="main2-image">
+          <img src={room} alt="Living Room" />
+          </div>
+        </div>
       </section>
     </>
   );
